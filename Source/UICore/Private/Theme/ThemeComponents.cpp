272c5395a2e3455d1e3c5f89ef31277c595e3890
Cuku <xhikab@gmail.com>
2025-03-26 00:07:06 +0100

    Load theme singletons

--- Source/UICore/Private/Theme/ThemeComponents.cpp
+++ Source/UICore/Private/Theme/ThemeComponents.cpp
@@ -1,11 +1,25 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Theme/ThemeComponents.h"
+#include "CoreMinimal.h"
 #include "flecs.h"
+#include "World.h"
 
 namespace UI {
 	void ThemeComponents::Register(flecs::world& world) {
-		world.component<UIScale>().member<float>("Value");		
+		world.component<FString>()
+			.opaque(flecs::String)
+			.serialize([](const flecs::serializer* s, const FString* data) {
+			const char* str = TCHAR_TO_UTF8(**data);
+			return s->value(flecs::String, &str);
+				})
+			.assign_string([](FString* data, const char* value) {
+			*data = UTF8_TO_TCHAR(value);
+				});
+
+		world.component<UIScale>().member<float>("Value");
+		world.component<TextFont>().member<FString>("Value");
+
+		World::DeserializeSingletons(world, "UIConfig");
 	}
 }
