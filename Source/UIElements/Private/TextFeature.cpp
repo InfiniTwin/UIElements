b276655d412adfd9098360658f4fa2a73510e5b9
Besjan <xhikab@gmail.com>
2025-05-18 00:22:14 +0200

    Revert "No need for all the components"
    This reverts commit 98903256167312947c688908e787b7c40ac6a113.

--- Source/UIElements/Private/TextFeature.cpp
+++ Source/UIElements/Private/TextFeature.cpp
@@ -80,6 +80,8 @@ namespace UIElements {
 		world.system("AddFontInfo")
 			.with(flecs::Prefab)
 			.with<Font>()
+			.with<FontFace>()
+			.with<FontSize>()
 			.without<FontInfo>()
 			.each([](flecs::entity e) { e.add<FontInfo>().disable<FontInfo>(); });
 
